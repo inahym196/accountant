fc56179f23af033e634a1c258e420a19d1ab0295
USER <user@USERnoMacBook-Pro.local>
2023-11-21 00:20:41 +0900

    DBは抽象化せずinterface層で扱う

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"net/http"
 
-	"github.com/inahym196/accountant/pkg/infra"
 	"github.com/inahym196/accountant/pkg/interface/database"
 	ai_handler "github.com/inahym196/accountant/pkg/interface/handler"
 	"github.com/inahym196/accountant/pkg/usecase"
 )
 
 func main() {
-	db_conn := infra.NewSQLiteConnector("./test.sqlite3")
+	db_conn := database.NewSQLiteConnector("./test.sqlite3")
 	repo := database.NewAccountItemRepository(db_conn.Conn)
 	i := usecase.NewAccountItemInteractor(repo)
 	handler := ai_handler.NewAccountItemHandler(i)
