00b4517b6a8a075d598c6f1bbec74cce2a10b5ca
USER <user@USERnoMacBook-Pro.local>
2023-11-21 01:00:51 +0900

    infra層の作成とhandlerのrename

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"net/http"
 
+	"github.com/inahym196/accountant/pkg/infra"
 	"github.com/inahym196/accountant/pkg/interface/database"
-	ai_handler "github.com/inahym196/accountant/pkg/interface/handler"
+	"github.com/inahym196/accountant/pkg/interface/http/controller"
 	"github.com/inahym196/accountant/pkg/usecase"
 )
 
 func main() {
-	db_conn := database.NewSQLiteConnector("./test.sqlite3")
+	db_conn := infra.NewSQLiteConnector("./test.sqlite3")
 	repo := database.NewAccountItemRepository(db_conn.Conn)
 	i := usecase.NewAccountItemInteractor(repo)
-	handler := ai_handler.NewAccountItemHandler(i)
+	handler := controller.NewAccountItemHandler(i)
 	http.HandleFunc("/", handler.Get)
 	http.ListenAndServe("localhost:8080", nil)
 }
