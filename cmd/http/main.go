cbcfa703ffbe597c2cfa6aff243ecaccabf6f0aa
USER <user@USERnoMacBook-Pro.local>
2023-11-26 14:22:16 +0900

    main setupをちょっといい感じにした

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -10,15 +10,15 @@ import (
 	"github.com/inahym196/accountant/pkg/util"
 )
 
-func newServer(path string) infra.Server {
+func setupAccountItem(path string) controller.AccountItemController {
 	db_conn := infra.NewSQLiteConnector(path).GetConn()
 	repo := database.NewAccountItemRepository(db_conn)
 	i := usecase.NewAccountItemInteractor(repo)
-	c := controller.NewAccountItemController(i)
-	return infra.NewServer(c)
+	return controller.NewAccountItemController(i)
 }
 
 func main() {
-	server := newServer(filepath.Join(util.ProjectRoot(), "./test.sqlite3"))
+	accountItem := setupAccountItem(filepath.Join(util.ProjectRoot(), "./test.sqlite3"))
+	server := infra.NewServer(accountItem)
 	server.Run("localhost:8080")
 }
